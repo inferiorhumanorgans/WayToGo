e29400825e99836d26941b76e9eb1dcb3b9c8f4e
Alex Zepeda <alex@inferiorhumanorgans.com>
2012-03-07 20:52:39 -0800

    More graceful vrsion increment.

--- mkversion.pl
+++ mkversion.pl
@@ -4,23 +4,34 @@ use warnings;
 
 my $version = 0;
 
+
+open(VERSION_FILE, "+<version.txt");
+$version = <VERSION_FILE>+1;
+truncate(VERSION_FILE, 0);
+seek(VERSION_FILE, 0, 0);
+print VERSION_FILE $version . "\n";
+close(VERSION_FILE);
+
 {
-	local $^I = '';
-	local @ARGV = ('AndroidManifest.xml');
-	while (<>) {
-		if (($_ =~ m/android:versionCode="(\d{2,})"/) && ($version == 0)) {
-			$version = $1+1;
-		}
-		s/android:versionCode="(\d{2,})"/android:versionCode="$version"/;
-		print;
+	open(INFILE, "<AndroidManifest.xml.in");
+	open(OUTFILE, ">AndroidManifest.xml");
+
+	while (<INFILE>) {
+		s/###/$version/g;
+		print OUTFILE $_;
 	}
+	close(INFILE);
+	close(OUTFILE);
 }
 
 {
-	local $^I = '';
-	local @ARGV = ('res/values/version.xml');
-	while (<>) {
-		s/(\d{2,})/$version/;
-		print;
+	open(INFILE, "<res/values/version.xml.in");
+	open(OUTFILE, ">res/values/version.xml");
+
+	while (<INFILE>) {
+		s/###/$version/g;
+		print OUTFILE $_;
 	}
+	close(INFILE);
+	close(OUTFILE);
 }
